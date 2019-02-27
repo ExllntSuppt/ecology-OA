3c9b43217304a4e14aa50cd3cc2d0a1f2d0adb8a
ExllntSupprt <w.dove17@yandex.com>
2019-02-27 00:54:53 -0800

    FileUtil.java
    FileUtil.java

--- FileUtil.java
+++ FileUtil.java
@@ -1,202 +1,114 @@
- 
-package weaver.admincenter.file;
+package com.converter.util;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.util.Iterator;
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.SAXReader;
-import org.dom4j.io.XMLWriter;
-import weaver.general.GCONST;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 
-public class FileUtil {
-	private static String codeModel = null;
-	public static String filePath = null;
+public class FileUtil
+{
+  public FileUtil() {}
+  
+  public static String getFilePrefix(String paramString)
+  {
+    int i = paramString.lastIndexOf(".");
+    return paramString.substring(0, i);
+  }
+  
+  public static String getFileSufix(String paramString) {
+    int i = paramString.lastIndexOf(".");
+    return paramString.substring(i + 1);
+  }
+  
+  public static void copyFile(String paramString1, String paramString2) throws java.io.FileNotFoundException {
+    File localFile1 = new File(paramString1);
+    File localFile2 = new File(paramString2);
+    FileInputStream localFileInputStream = new FileInputStream(localFile1);
+    FileOutputStream localFileOutputStream = new FileOutputStream(localFile2);
+    int i = 0;
+    try {
+      while ((i = localFileInputStream.read()) != -1)
+        localFileOutputStream.write(i);
+      return;
+    } catch (IOException localIOException2) {
+      localIOException2.printStackTrace();
+    } finally {
+      try {
+        localFileInputStream.close();
+        localFileOutputStream.close();
+      } catch (IOException localIOException4) {
+        localIOException4.printStackTrace();
+      }
+    }
+  }
+  
+  public static boolean deleteDir(File paramFile)
+  {
+    if (paramFile.isDirectory()) {
+      String[] arrayOfString = paramFile.list();
+      for (int i = 0; i < arrayOfString.length; i++) {
+        boolean bool = deleteDir(new File(paramFile, arrayOfString[i]));
+        if (!bool) {
+          return false;
+        }
+      }
+    }
+    
+    return paramFile.delete();
+  }
+  
 
-	public FileUtil() {
-	}
+  public static String getSHA1(String paramString)
+    throws OutOfMemoryError, IOException
+  {
+    File localFile = new File(paramString);
+    FileInputStream localFileInputStream = new FileInputStream(localFile);
+    try
+    {
+      MessageDigest localMessageDigest = MessageDigest.getInstance("SHA-1");
+      
+      byte[] arrayOfByte = new byte[10485760];
+      int i = 0;
+      
+      while ((i = localFileInputStream.read(arrayOfByte)) > 0)
+      {
+        localMessageDigest.update(arrayOfByte, 0, i);
+      }
+      
 
-	public static boolean dispatch(String paramString1, String paramString2) {
-		return dispatch(paramString1, paramString2, null, "80");
-	}
 
-	public static boolean dispatch(String paramString1, String paramString2,
-			String paramString3) {
-		return dispatch(paramString1, paramString2, paramString3, "80");
-	}
-
-	public static boolean dispatch(String paramString1, String paramString2,
-			String paramString3, String paramString4) {
-		return false;
-	}
-
-	public static boolean deploy(String paramString1, String paramString2,
-			String paramString3) {
-		try {
-			return copyFile(new File(paramString2 + "/" + paramString1
-					+ ".class"), new File(paramString3 + "/" + paramString1
-					+ ".class"));
-		} catch (Exception localException) {
-			localException.printStackTrace();
-		}
-		return false;
-	}
-
-	public static boolean copyFile(File paramFile1, File paramFile2) {
-		BufferedInputStream localBufferedInputStream = null;
-		BufferedOutputStream localBufferedOutputStream = null;
-		try {
-			localBufferedInputStream = new BufferedInputStream(
-					new FileInputStream(paramFile1));
-
-			localBufferedOutputStream = new BufferedOutputStream(
-					new FileOutputStream(paramFile2));
-
-			byte[] arrayOfByte = new byte['?'];
-			while (( localBufferedInputStream.read(arrayOfByte)) != -1) {
-				int i = localBufferedInputStream.read(arrayOfByte);
-				localBufferedOutputStream.write(arrayOfByte, 0, i);
-			}
-			localBufferedOutputStream.flush();
-			return true;
-		} catch (Exception localException1) {
-			int i;
-			return false;
-		} finally {
-			if (localBufferedInputStream != null) {
-				try {
-					localBufferedInputStream.close();
-				} catch (Exception localException6) {
-				}
-			}
-			if (localBufferedOutputStream != null) {
-				try {
-					localBufferedOutputStream.close();
-				} catch (Exception localException7) {
-				}
-			}
-		}
-	}
-
-	public static boolean createCode(String paramString) {
-		if (codeModel == null) {
-			boolean bool = loadBaseAction();
-			if (!bool) {
-				return false;
-			}
-		}
-		String str = codeModel.replaceAll("BaseAction", paramString);
-		try {
-			BufferedWriter localBufferedWriter = new BufferedWriter(
-					new FileWriter(new File(filePath + "/" + paramString
-							+ ".java")));
-			localBufferedWriter.write(str);
-			localBufferedWriter.close();
-			return true;
-		} catch (Exception localException) {
-			localException.printStackTrace();
-		}
-		return false;
-	}
-
-	public static boolean addOrUpdateXml(String paramString1,
-			String paramString2) {
-		try {
-			XMLWriter localXMLWriter = null;
-			SAXReader localSAXReader = new SAXReader();
-			File localFile = new File(paramString2
-					+ "WEB-INF/service/action.xml");
-			OutputFormat localOutputFormat = OutputFormat.createPrettyPrint();
-			localOutputFormat.setEncoding(GCONST.XML_UTF8);
-			Document localDocument;
-			Element localElement1;
-			Object localObject;
-			Element localElement2;
-			Element localElement3;
-			if (localFile.exists()) {
-				localDocument = localSAXReader.read(localFile);
-				localElement1 = localDocument.getRootElement();
-				for (localObject = localElement1
-						.elementIterator("service-point"); ((Iterator) localObject)
-						.hasNext();) {
-					localElement2 = (Element) ((Iterator) localObject).next();
-					if (localElement2.attributeValue("id").equals(paramString1)) {
-						return true;
-					}
-				}
-				localObject = localElement1.addElement("service-point");
-				((Element) localObject).addAttribute("id", paramString1);
-				((Element) localObject).addAttribute("interface",
-						"weaver.interfaces.workflow.action.Action");
-				localElement2 = ((Element) localObject)
-						.addElement("invoke-factory");
-				localElement3 = localElement2.addElement("construct");
-				localElement3.addAttribute("class",
-						"weaver.interfaces.workflow.action." + paramString1);
-
-				localXMLWriter = new XMLWriter(new FileWriter(localFile),
-						localOutputFormat);
-				localXMLWriter.write(localDocument);
-				localXMLWriter.close();
-			} else {
-				localDocument = DocumentHelper.createDocument();
-				localElement1 = localDocument.addElement("module");
-				localElement1.addAttribute("id", "action");
-				localElement1.addAttribute("version", "1.0.0");
-				localObject = localElement1.addElement("service-point");
-				((Element) localObject).addAttribute("id", paramString1);
-				((Element) localObject).addAttribute("interface",
-						"weaver.interfaces.workflow.action.Action");
-				localElement2 = ((Element) localObject)
-						.addElement("invoke-factory");
-				localElement3 = localElement2.addElement("construct");
-				localElement3.addAttribute("class",
-						"weaver.interfaces.workflow.action." + paramString1);
-
-				localXMLWriter = new XMLWriter(new FileWriter(localFile),
-						localOutputFormat);
-				localXMLWriter.write(localDocument);
-				localXMLWriter.close();
-			}
-		} catch (Exception localException) {
-			localException.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-
-	private static boolean loadBaseAction() {
-		try {
-			String str1 = "UTF-8";
-			codeModel = "";
-			File localFile = new File(filePath + "/BaseAction.java");
-			if ((localFile.isFile()) && (localFile.exists())) {
-				InputStreamReader localInputStreamReader = new InputStreamReader(
-						new FileInputStream(localFile), str1);
-
-				BufferedReader localBufferedReader = new BufferedReader(
-						localInputStreamReader);
-				String str2 = null;
-				while ((str2 = localBufferedReader.readLine()) != null) {
-					codeModel = codeModel + "\n" + str2;
-				}
-				localInputStreamReader.close();
-				return true;
-			}
-			return false;
-		} catch (Exception localException) {
-			localException.printStackTrace();
-		}
-		return false;
-	}
+      SHA1Util localSHA1Util = new SHA1Util();
+      return localSHA1Util.getDigestOfString(localMessageDigest.digest());
+    }
+    catch (NoSuchAlgorithmException localNoSuchAlgorithmException) {
+      localNoSuchAlgorithmException.printStackTrace();
+    }
+    catch (OutOfMemoryError localOutOfMemoryError)
+    {
+      throw localOutOfMemoryError;
+    } finally {
+      localFileInputStream.close();
+    }
+    return null;
+  }
+  
+  public static int getPdfPage(String paramString) {
+    int i = 0;
+    try {
+      PDDocument localPDDocument = PDDocument.load(paramString);
+      List localList = localPDDocument.getDocumentCatalog().getAllPages();
+      i = localList.size();
+    }
+    catch (Exception localException) {
+      localException.printStackTrace();
+    }
+    return i;
+  }
+  
+  public static void main(String[] paramArrayOfString) {}
 }
