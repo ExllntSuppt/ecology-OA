2d502948fe0159482bdddbe0765874ec90ba3a64
ExllntSupprt <w.dove17@yandex.com>
2019-03-20 09:41:32 -0700

    ApplyUserVO.java
    ApplyUserVO.java

---
+++ ApplyUserVO.java
@@ -0,0 +1,241 @@
+/*
+ *  
+ */
+package weaver.apply;
+
+public class ApplyUserVO {
+    private String id;
+    private String name;
+    private String areacode;
+    private String telephone;
+    private String fax;
+    private String email;
+    private String web;
+    private String company;
+    private String title;
+    private String industry_sector;
+    private String address;
+    private String email_alert;
+    private String care_product;
+    private String care_apply;
+    private String type;
+    private String province;
+    private String city;
+    private String companyEn;
+    private String zipcode;
+    private String customerDesc;
+    private String customerSize;
+    private String startDate;
+    private String nameTitle;
+    private String mobile;
+    private String remark;
+    private String email2;
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getAreacode() {
+        return this.areacode;
+    }
+
+    public String getCare_apply() {
+        return this.care_apply;
+    }
+
+    public String getCare_product() {
+        return this.care_product;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getCompany() {
+        return this.company;
+    }
+
+    public String getCompanyEn() {
+        return this.companyEn;
+    }
+
+    public String getCustomerDesc() {
+        return this.customerDesc;
+    }
+
+    public String getCustomerSize() {
+        return this.customerSize;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getEmail_alert() {
+        return this.email_alert;
+    }
+
+    public String getEmail2() {
+        return this.email2;
+    }
+
+    public String getFax() {
+        return this.fax;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getIndustry_sector() {
+        return this.industry_sector;
+    }
+
+    public String getMobile() {
+        return this.mobile;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNameTitle() {
+        return this.nameTitle;
+    }
+
+    public String getProvince() {
+        return this.province;
+    }
+
+    public String getRemark() {
+        return this.remark;
+    }
+
+    public String getStartDate() {
+        return this.startDate;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getWeb() {
+        return this.web;
+    }
+
+    public String getZipcode() {
+        return this.zipcode;
+    }
+
+    public void setAddress(String string) {
+        this.address = string;
+    }
+
+    public void setAreacode(String string) {
+        this.areacode = string;
+    }
+
+    public void setCare_apply(String string) {
+        this.care_apply = string;
+    }
+
+    public void setCare_product(String string) {
+        this.care_product = string;
+    }
+
+    public void setCity(String string) {
+        this.city = string;
+    }
+
+    public void setCompany(String string) {
+        this.company = string;
+    }
+
+    public void setCompanyEn(String string) {
+        this.companyEn = string;
+    }
+
+    public void setCustomerDesc(String string) {
+        this.customerDesc = string;
+    }
+
+    public void setCustomerSize(String string) {
+        this.customerSize = string;
+    }
+
+    public void setEmail(String string) {
+        this.email = string;
+    }
+
+    public void setEmail_alert(String string) {
+        this.email_alert = string;
+    }
+
+    public void setEmail2(String string) {
+        this.email2 = string;
+    }
+
+    public void setFax(String string) {
+        this.fax = string;
+    }
+
+    public void setId(String string) {
+        this.id = string;
+    }
+
+    public void setIndustry_sector(String string) {
+        this.industry_sector = string;
+    }
+
+    public void setMobile(String string) {
+        this.mobile = string;
+    }
+
+    public void setName(String string) {
+        this.name = string;
+    }
+
+    public void setNameTitle(String string) {
+        this.nameTitle = string;
+    }
+
+    public void setProvince(String string) {
+        this.province = string;
+    }
+
+    public void setRemark(String string) {
+        this.remark = string;
+    }
+
+    public void setStartDate(String string) {
+        this.startDate = string;
+    }
+
+    public void setTelephone(String string) {
+        this.telephone = string;
+    }
+
+    public void setTitle(String string) {
+        this.title = string;
+    }
+
+    public void setType(String string) {
+        this.type = string;
+    }
+
+    public void setWeb(String string) {
+        this.web = string;
+    }
+
+    public void setZipcode(String string) {
+        this.zipcode = string;
+    }
+}
