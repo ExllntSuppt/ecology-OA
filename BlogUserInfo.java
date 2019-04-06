19d2f9a9aa3cb475dd9deb0c715d5efcdf4dc9b0
ExllntSupprt <w.dove17@yandex.com>
2019-04-06 01:58:21 -0700

    BlogUserInfo.java
    BlogUserInfo.java

---
+++ BlogUserInfo.java
@@ -0,0 +1,106 @@
+/*
+ *  
+ */
+package weaver.blog.webservices;
+
+public class BlogUserInfo {
+    String userid;
+    String username;
+    String isnew;
+    String deptName;
+    String subName;
+    String jobtitle;
+    String imageUrl;
+    String blogCount;
+    String attentionCount;
+    String attentionMeCount;
+    String sex;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String string) {
+        this.username = string;
+    }
+
+    public String getIsnew() {
+        return this.isnew;
+    }
+
+    public void setIsnew(String string) {
+        this.isnew = string;
+    }
+
+    public String getDeptName() {
+        return this.deptName;
+    }
+
+    public void setDeptName(String string) {
+        this.deptName = string;
+    }
+
+    public String getJobtitle() {
+        return this.jobtitle;
+    }
+
+    public void setJobtitle(String string) {
+        this.jobtitle = string;
+    }
+
+    public String getBlogCount() {
+        return this.blogCount;
+    }
+
+    public void setBlogCount(String string) {
+        this.blogCount = string;
+    }
+
+    public String getAttentionCount() {
+        return this.attentionCount;
+    }
+
+    public void setAttentionCount(String string) {
+        this.attentionCount = string;
+    }
+
+    public String getAttentionMeCount() {
+        return this.attentionMeCount;
+    }
+
+    public void setAttentionMeCount(String string) {
+        this.attentionMeCount = string;
+    }
+
+    public String getUserid() {
+        return this.userid;
+    }
+
+    public void setUserid(String string) {
+        this.userid = string;
+    }
+
+    public String getSubName() {
+        return this.subName;
+    }
+
+    public void setSubName(String string) {
+        this.subName = string;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public void setImageUrl(String string) {
+        this.imageUrl = string;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public void setSex(String string) {
+        this.sex = string;
+    }
+}
