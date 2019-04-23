0e7c90912223fbf46cefa632ab040ee27d503dc8
ExllntSupprt <w.dove17@yandex.com>
2019-04-23 08:05:06 -0700

    ShareInnerDoc
    ShareInnerDoc

---
+++ ShareInnerDoc.java
@@ -0,0 +1,11 @@
+package com.weaver.ecology.search.model;
+
+import com.weaver.ecology.search.model.base.AbstractShareInnerDoc;
+
+public class ShareInnerDoc
+  extends AbstractShareInnerDoc
+{
+  private static final long serialVersionUID = -647270797144689170L;
+  
+  public ShareInnerDoc() {}
+}
