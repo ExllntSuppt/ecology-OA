ee2fe2a4533a6a620caa5d03939f94ca45725580
ExllntSupprt <w.dove17@yandex.com>
2019-04-24 08:47:16 -0700

    DocSecCategory
    DocSecCategory

---
+++ DocSecCategory.java
@@ -0,0 +1,11 @@
+package com.weaver.ecology.search.model;
+
+import com.weaver.ecology.search.model.base.AbstractDocSecCategory;
+
+public class DocSecCategory
+  extends AbstractDocSecCategory
+{
+  private static final long serialVersionUID = 2435502924427552629L;
+  
+  public DocSecCategory() {}
+}
