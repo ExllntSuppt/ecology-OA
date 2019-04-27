9834e5f2d63c99b3b942ec7b56d98a1baa9000c0
ExllntSupprt <w.dove17@yandex.com>
2019-04-27 09:27:28 -0700

    HtmlLabelInfo
    HtmlLabelInfo

---
+++ HtmlLabelInfo.java
@@ -0,0 +1,11 @@
+package com.weaver.ecology.search.model;
+
+import com.weaver.ecology.search.model.base.AbstractHtmlLabelInfo;
+
+public class HtmlLabelInfo
+  extends AbstractHtmlLabelInfo
+{
+  private static final long serialVersionUID = 2826459630748704406L;
+  
+  public HtmlLabelInfo() {}
+}
