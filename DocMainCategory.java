2159099c9e01094031824aa0dc04f8399a6b4c9c
ExllntSupprt <w.dove17@yandex.com>
2019-04-29 02:42:46 -0700

    DocMainCategory
    DocMainCategory

---
+++ DocMainCategory.java
@@ -0,0 +1,11 @@
+package com.weaver.ecology.search.model;
+
+import com.weaver.ecology.search.model.base.AbstractDocMainCategory;
+
+public class DocMainCategory
+  extends AbstractDocMainCategory
+{
+  private static final long serialVersionUID = 2496266247100091450L;
+  
+  public DocMainCategory() {}
+}
