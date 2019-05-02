1c2c0f042aa1d95daa8121a56506ffdcd8121715
ExllntSupprt <w.dove17@yandex.com>
2019-05-02 09:47:26 -0700

    DocSubCategoryDocSubCategory
    DocSubCategory

---
+++ DocSubCategory.java
@@ -0,0 +1,11 @@
+package com.weaver.ecology.search.model;
+
+import com.weaver.ecology.search.model.base.AbstractDocSubCategory;
+
+public class DocSubCategory
+  extends AbstractDocSubCategory
+{
+  private static final long serialVersionUID = -1232750606778802859L;
+  
+  public DocSubCategory() {}
+}
