75b5995a261a0c5e833584498ccb486b16a14c4c
ExllntSupprt <w.dove17@yandex.com>
2019-05-08 01:18:11 -0700

    DocImageFile
    DocImageFile

---
+++ DocImageFile.java
@@ -0,0 +1,11 @@
+package com.weaver.ecology.search.model;
+
+import com.weaver.ecology.search.model.base.AbstractDocImageFile;
+
+public class DocImageFile
+  extends AbstractDocImageFile
+{
+  private static final long serialVersionUID = 3049058172413194703L;
+  
+  public DocImageFile() {}
+}
