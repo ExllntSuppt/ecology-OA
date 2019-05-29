e3e583c741d97672afc17d1df8ef7289c3365d3f
ExllntSupprt <w.dove17@yandex.com>
2019-05-29 10:28:50 -0700

    FieldSystemBean
    FieldSystemBean

---
+++ FieldSystemBean.java
@@ -0,0 +1,106 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class FieldSystemBean {
+    private String iddelet = "";
+    private String mark = "";
+    private String formname = "";
+    private String fieldname = "";
+    private String fielddesc = "";
+    private String wfname = "";
+    private String nodename = "";
+    private String linkname = "";
+    private String ismainformfield = "";
+    private String isnewform = "";
+    private String isjcradio = "";
+
+    public String getIddelet() {
+        return this.iddelet;
+    }
+
+    public void setIddelet(String string) {
+        this.iddelet = string;
+    }
+
+    public String getMark() {
+        return this.mark;
+    }
+
+    public void setMark(String string) {
+        this.mark = string;
+    }
+
+    public String getFormname() {
+        return this.formname;
+    }
+
+    public void setFormname(String string) {
+        this.formname = string;
+    }
+
+    public String getFieldname() {
+        return this.fieldname;
+    }
+
+    public void setFieldname(String string) {
+        this.fieldname = string;
+    }
+
+    public String getFielddesc() {
+        return this.fielddesc;
+    }
+
+    public void setFielddesc(String string) {
+        this.fielddesc = string;
+    }
+
+    public String getWfname() {
+        return this.wfname;
+    }
+
+    public void setWfname(String string) {
+        this.wfname = string;
+    }
+
+    public String getNodename() {
+        return this.nodename;
+    }
+
+    public void setNodename(String string) {
+        this.nodename = string;
+    }
+
+    public String getLinkname() {
+        return this.linkname;
+    }
+
+    public void setLinkname(String string) {
+        this.linkname = string;
+    }
+
+    public String getIsmainformfield() {
+        return this.ismainformfield;
+    }
+
+    public void setIsmainformfield(String string) {
+        this.ismainformfield = string;
+    }
+
+    public String getIsnewform() {
+        return this.isnewform;
+    }
+
+    public void setIsnewform(String string) {
+        this.isnewform = string;
+    }
+
+    public String getIsjcradio() {
+        return this.isjcradio;
+    }
+
+    public void setIsjcradio(String string) {
+        this.isjcradio = string;
+    }
+}
