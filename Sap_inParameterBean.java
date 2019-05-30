ff84ecf4199e894cc2a4aa852cb47f5b8d73db4c
ExllntSupprt <w.dove17@yandex.com>
2019-05-30 16:09:30 -0700

    Sap_inParameterBean
    Sap_inParameterBean

---
+++ Sap_inParameterBean.java
@@ -0,0 +1,133 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class Sap_inParameterBean {
+    private String id;
+    private String baseid;
+    private String sapfield;
+    private String oafield;
+    private String constant;
+    private String ordernum;
+    private String ismainfield;
+    private String fromfieldid;
+    private String isbill;
+    private String showname;
+    private String oadesc;
+    private String isshow;
+    private String isrdonly;
+    private String orderfield;
+
+    public String getShowname() {
+        return this.showname;
+    }
+
+    public void setShowname(String string) {
+        this.showname = string;
+    }
+
+    public String getOadesc() {
+        return this.oadesc;
+    }
+
+    public void setOadesc(String string) {
+        this.oadesc = string;
+    }
+
+    public String getIsshow() {
+        return this.isshow;
+    }
+
+    public void setIsshow(String string) {
+        this.isshow = string;
+    }
+
+    public String getIsrdonly() {
+        return this.isrdonly;
+    }
+
+    public void setIsrdonly(String string) {
+        this.isrdonly = string;
+    }
+
+    public String getOrderfield() {
+        return this.orderfield;
+    }
+
+    public void setOrderfield(String string) {
+        this.orderfield = string;
+    }
+
+    public String getIsbill() {
+        return this.isbill;
+    }
+
+    public void setIsbill(String string) {
+        this.isbill = string;
+    }
+
+    public String getOrdernum() {
+        return this.ordernum;
+    }
+
+    public void setOrdernum(String string) {
+        this.ordernum = string;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String string) {
+        this.id = string;
+    }
+
+    public String getBaseid() {
+        return this.baseid;
+    }
+
+    public void setBaseid(String string) {
+        this.baseid = string;
+    }
+
+    public String getSapfield() {
+        return this.sapfield;
+    }
+
+    public void setSapfield(String string) {
+        this.sapfield = string;
+    }
+
+    public String getOafield() {
+        return this.oafield;
+    }
+
+    public void setOafield(String string) {
+        this.oafield = string;
+    }
+
+    public String getConstant() {
+        return this.constant;
+    }
+
+    public void setConstant(String string) {
+        this.constant = string;
+    }
+
+    public String getIsmainfield() {
+        return this.ismainfield;
+    }
+
+    public void setIsmainfield(String string) {
+        this.ismainfield = string;
+    }
+
+    public String getFromfieldid() {
+        return this.fromfieldid;
+    }
+
+    public void setFromfieldid(String string) {
+        this.fromfieldid = string;
+    }
+}
