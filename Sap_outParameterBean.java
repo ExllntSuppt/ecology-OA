f526c2595909f28c5fc93cbf822d57cc7b45e66a
ExllntSupprt <w.dove17@yandex.com>
2019-06-05 03:24:24 -0700

    Sap_outParameterBean
    Sap_outParameterBean

---
+++ Sap_outParameterBean.java
@@ -0,0 +1,106 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class Sap_outParameterBean {
+    private String id;
+    private String baseid;
+    private String sapfield;
+    private String showname;
+    private String display;
+    private String ordernum;
+    private String oafield;
+    private String ismainfield;
+    private String fromfieldid;
+    private String isbill;
+    private String oadesc;
+
+    public String getOadesc() {
+        return this.oadesc;
+    }
+
+    public void setOadesc(String string) {
+        this.oadesc = string;
+    }
+
+    public String getIsbill() {
+        return this.isbill;
+    }
+
+    public void setIsbill(String string) {
+        this.isbill = string;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String string) {
+        this.id = string;
+    }
+
+    public String getBaseid() {
+        return this.baseid;
+    }
+
+    public void setBaseid(String string) {
+        this.baseid = string;
+    }
+
+    public String getSapfield() {
+        return this.sapfield;
+    }
+
+    public void setSapfield(String string) {
+        this.sapfield = string;
+    }
+
+    public String getShowname() {
+        return this.showname;
+    }
+
+    public void setShowname(String string) {
+        this.showname = string;
+    }
+
+    public String getDisplay() {
+        return this.display;
+    }
+
+    public void setDisplay(String string) {
+        this.display = string;
+    }
+
+    public String getOrdernum() {
+        return this.ordernum;
+    }
+
+    public void setOrdernum(String string) {
+        this.ordernum = string;
+    }
+
+    public String getOafield() {
+        return this.oafield;
+    }
+
+    public void setOafield(String string) {
+        this.oafield = string;
+    }
+
+    public String getIsmainfield() {
+        return this.ismainfield;
+    }
+
+    public void setIsmainfield(String string) {
+        this.ismainfield = string;
+    }
+
+    public String getFromfieldid() {
+        return this.fromfieldid;
+    }
+
+    public void setFromfieldid(String string) {
+        this.fromfieldid = string;
+    }
+}
