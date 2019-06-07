795c9f0420ae22e285bd450f2b09b490cf9a3855
ExllntSupprt <w.dove17@yandex.com>
2019-06-07 01:14:45 -0700

    outparaprocessBean
    outparaprocessBean

---
+++ Sap_outparaprocessBean.java
@@ -0,0 +1,106 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class Sap_outparaprocessBean {
+    private String id;
+    private String baseid;
+    private String nameid;
+    private String name;
+    private String sapfield;
+    private String oafield;
+    private String constant;
+    private String ordernum;
+    private String ismainfield;
+    private String fromfieldid;
+    private String isbill;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String string) {
+        this.id = string;
+    }
+
+    public String getBaseid() {
+        return this.baseid;
+    }
+
+    public void setBaseid(String string) {
+        this.baseid = string;
+    }
+
+    public String getNameid() {
+        return this.nameid;
+    }
+
+    public void setNameid(String string) {
+        this.nameid = string;
+    }
+
+    public String getSapfield() {
+        return this.sapfield;
+    }
+
+    public void setSapfield(String string) {
+        this.sapfield = string;
+    }
+
+    public String getOafield() {
+        return this.oafield;
+    }
+
+    public void setOafield(String string) {
+        this.oafield = string;
+    }
+
+    public String getConstant() {
+        return this.constant;
+    }
+
+    public void setConstant(String string) {
+        this.constant = string;
+    }
+
+    public String getOrdernum() {
+        return this.ordernum;
+    }
+
+    public void setOrdernum(String string) {
+        this.ordernum = string;
+    }
+
+    public String getIsmainfield() {
+        return this.ismainfield;
+    }
+
+    public void setIsmainfield(String string) {
+        this.ismainfield = string;
+    }
+
+    public String getFromfieldid() {
+        return this.fromfieldid;
+    }
+
+    public void setFromfieldid(String string) {
+        this.fromfieldid = string;
+    }
+
+    public String getIsbill() {
+        return this.isbill;
+    }
+
+    public void setIsbill(String string) {
+        this.isbill = string;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String string) {
+        this.name = string;
+    }
+}
