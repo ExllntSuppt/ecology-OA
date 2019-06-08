8f658d0b2e66a31faaf723076a44da9ab42ca10e
ExllntSupprt <w.dove17@yandex.com>
2019-06-08 06:34:38 -0700

    Sap_outTableBean.java
    Sap_outTableBean.java

---
+++ Sap_outTableBean.java
@@ -0,0 +1,151 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class Sap_outTableBean {
+    private String id;
+    private String baseid;
+    private String name;
+    private String nameid;
+    private String sapfield;
+    private String showname;
+    private String display;
+    private String search;
+    private String primarykey;
+    private String ordernum;
+    private String orderGroupnum;
+    private String oafield;
+    private String ismainfield;
+    private String fromfieldid;
+    private String isbill;
+    private String oadesc;
+
+    public String getOadesc() {
+        return this.oadesc;
+    }
+
+    public void setOadesc(String string) {
+        this.oadesc = string;
+    }
+
+    public String getDisplay() {
+        return this.display;
+    }
+
+    public void setDisplay(String string) {
+        this.display = string;
+    }
+
+    public String getSearch() {
+        return this.search;
+    }
+
+    public void setSearch(String string) {
+        this.search = string;
+    }
+
+    public String getPrimarykey() {
+        return this.primarykey;
+    }
+
+    public void setPrimarykey(String string) {
+        this.primarykey = string;
+    }
+
+    public String getOrdernum() {
+        return this.ordernum;
+    }
+
+    public void setOrdernum(String string) {
+        this.ordernum = string;
+    }
+
+    public String getOrderGroupnum() {
+        return this.orderGroupnum;
+    }
+
+    public void setOrderGroupnum(String string) {
+        this.orderGroupnum = string;
+    }
+
+    public String getOafield() {
+        return this.oafield;
+    }
+
+    public void setOafield(String string) {
+        this.oafield = string;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String string) {
+        this.id = string;
+    }
+
+    public String getBaseid() {
+        return this.baseid;
+    }
+
+    public void setBaseid(String string) {
+        this.baseid = string;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String string) {
+        this.name = string;
+    }
+
+    public String getSapfield() {
+        return this.sapfield;
+    }
+
+    public void setSapfield(String string) {
+        this.sapfield = string;
+    }
+
+    public String getShowname() {
+        return this.showname;
+    }
+
+    public void setShowname(String string) {
+        this.showname = string;
+    }
+
+    public String getIsmainfield() {
+        return this.ismainfield;
+    }
+
+    public void setIsmainfield(String string) {
+        this.ismainfield = string;
+    }
+
+    public String getFromfieldid() {
+        return this.fromfieldid;
+    }
+
+    public void setFromfieldid(String string) {
+        this.fromfieldid = string;
+    }
+
+    public String getNameid() {
+        return this.nameid;
+    }
+
+    public void setNameid(String string) {
+        this.nameid = string;
+    }
+
+    public String getIsbill() {
+        return this.isbill;
+    }
+
+    public void setIsbill(String string) {
+        this.isbill = string;
+    }
+}
