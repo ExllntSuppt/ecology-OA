41083099a49b6a9fd7993b9e8768007fdf59e186
ExllntSupprt <w.dove17@yandex.com>
2019-06-11 06:41:33 -0700

    SapjarBean
    SapjarBean

---
+++ SapjarBean.java
@@ -0,0 +1,43 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class SapjarBean {
+    private String jardesc;
+    private String jarid;
+    private String jarname;
+    private String jarurl;
+
+    public String getJardesc() {
+        return this.jardesc;
+    }
+
+    public void setJardesc(String string) {
+        this.jardesc = string;
+    }
+
+    public String getJarid() {
+        return this.jarid;
+    }
+
+    public void setJarid(String string) {
+        this.jarid = string;
+    }
+
+    public String getJarname() {
+        return this.jarname;
+    }
+
+    public void setJarname(String string) {
+        this.jarname = string;
+    }
+
+    public String getJarurl() {
+        return this.jarurl;
+    }
+
+    public void setJarurl(String string) {
+        this.jarurl = string;
+    }
+}
