4528e4ef958809095e3f708736bb285edccd49c8
ExllntSupprt <w.dove17@yandex.com>
2019-06-26 05:15:19 -0700

    Int_authorizeDetaRightBean
    Int_authorizeDetaRightBean

---
+++ Int_authorizeDetaRightBean.java
@@ -0,0 +1,43 @@
+/*
+ *  
+ */
+package com.weaver.integration.entity;
+
+public class Int_authorizeDetaRightBean {
+    private String id;
+    private String rightid;
+    private String isinclude;
+    private String value;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String string) {
+        this.id = string;
+    }
+
+    public String getRightid() {
+        return this.rightid;
+    }
+
+    public void setRightid(String string) {
+        this.rightid = string;
+    }
+
+    public String getIsinclude() {
+        return this.isinclude;
+    }
+
+    public void setIsinclude(String string) {
+        this.isinclude = string;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String string) {
+        this.value = string;
+    }
+}
