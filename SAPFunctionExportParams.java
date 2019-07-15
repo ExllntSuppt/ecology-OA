90c050fbb80e24ed32572b6462a8002c182cb21f
ExllntSupprt <w.dove17@yandex.com>
2019-07-15 03:53:22 -0700

    SAPFunctionExportParams
    SAPFunctionExportParams

---
+++ SAPFunctionExportParams.java
@@ -0,0 +1,13 @@
+/*
+ *  
+ * 
+ *  
+ *  com.weaver.integration.datesource.SAPFunctionParams
+ */
+package com.weaver.integration.datesource;
+
+import com.weaver.integration.datesource.SAPFunctionParams;
+
+public class SAPFunctionExportParams
+extends SAPFunctionParams {
+}
